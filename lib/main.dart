e9954d1163ae766449ac6886512b302b245807b0
Kevin Bowe <kevinbowe1999@gmail.com>
2020-01-22 23:26:52 -0500

    Add Formatting to Cards
    AddTextFields that take user data in order to create new cards in the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -16,56 +17,117 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
-    Transaction(id: 't1', title: 'New Shoes', cost: 69.99, dateTransaction: DateTime.now()),
-    Transaction(id: 't2', title: 'New Headphones', cost: 13.99, dateTransaction: DateTime.now()),
-
-
+    Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        cost: 69.99,
+        dateTransaction: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'New Headphones',
+        cost: 13.99,
+        dateTransaction: DateTime.now()),
   ];
+
+  String titleInput;
+  String costInput;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.lightBlue,
-              child: Text(
-                'Chart',
-                style: TextStyle(
-                  color: Colors.white
-                )),
-              elevation: 5,
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.lightBlue,
+                child: Text('Chart', style: TextStyle(color: Colors.white)),
+                elevation: 5,
+              ),
             ),
-          ),
-          Column(
-            children: transactions.map((tx){
-              return Card(
-                child: Row(
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
-                    Container(
-                      child: Text(tx.cost.toString()),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      onChanged: (value){
+                        titleInput = value;
+                      },
                     ),
-                    Column(
-                      children: <Widget>[
-                        Text(tx.title),
-                        Text(tx.dateTransaction.toString())
-                      ],
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      onChanged: (value) {
+                        costInput = value;
+                        },
+                    ),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.lightGreen,
+                      onPressed: (){
+                        print(titleInput);
+                        print(costInput);
+                      },
                     )
                   ],
                 ),
-              );
-            }).toList(),
-          )
-        ],
-      )
-      );
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.lightGreen,
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.cost}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.lightGreen),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.dateTransaction),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
-
